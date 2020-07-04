8fff2b2df3baebf672d994141957ad5e15e22d81
yanaloz <yanaloz@yandex.ru>
2020-07-04 19:11:37 +0300

    2

--- src/components/productList.vue
+++ src/components/productList.vue
@@ -12,29 +12,15 @@
       </h3>
 
       <span class="catalog__price">
-              {{product.price}} ₽
-            </span>
+          {{product.price}} ₽
+      </span>
 
       <ul class="colors colors--black">
-        <li class="colors__item">
+        <li class="colors__item" v-for="(color, index) in product.colors" :key="index">
           <label class="colors__label">
-            <!--<input class="colors__radio sr-only" name="color-1" value="#73B6EA" checked="">-->
-            <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-          </label>
-        </li>
-        <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-            <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-          </label>
-        </li>
-        <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-            <span class="colors__value" style="background-color: #222;">
-                  </span>
+            <input class="colors__radio sr-only" type="radio" name="color-1" :value="color.color">
+            <span class="colors__value" :style='{backgroundColor: color}'>
+            </span>
           </label>
         </li>
       </ul>
@@ -44,7 +30,7 @@
 </template>
 
 <script>
-  export default {
-    props:['products']
-  }
+export default {
+  props: { products: Array },
+};
 </script>
--- src/data/products.js
+++ src/data/products.js
@@ -3,60 +3,96 @@ export default [
     title: 'Радионяня Motorola MBP16',
     price: '3690',
     image: '/img/radio.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
     price: '5660',
     image: '/img/toothbrush.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
     price: '21790',
     image: '/img/phone.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Электроскейт Razor Cruiser',
     price: '24690',
     image: '/img/board.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
     price: '14960',
     image: '/img/phone-2.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Смартфон Xiaomi Redmi 6/128GB',
     price: '8690',
     image: '/img/phone-3.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Электрический дрифт-карт Razor Crazy Cart',
     price: '39900',
     image: '/img/bicycle.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Гироскутер Razor Hovertrax 2.0',
     price: '34900',
     image: '/img/wheels.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Детский трюковой самокат Razor Grom',
     price: '6690',
     image: '/img/scooter.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Роллерсёрф Razor RipStik Air Pro',
     price: '6690',
     image: '/img/ripstik.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Наушники AirPods с беспроводным зарядным футляром',
     price: '16560',
     image: '/img/airpods.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
   {
     title: 'Наушники Sony',
     price: '30690',
     image: '/img/headphones.jpg',
+    colors: [
+      '#73B6EA', '#8BE000', '#222',
+    ],
   },
 ];
